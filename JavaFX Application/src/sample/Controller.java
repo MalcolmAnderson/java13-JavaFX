1281d35be54a41354b8c1dbb424980228a62a152
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-08-02 11:16:11 -0700

    make setscale work

--- JavaFX Application/src/sample/Controller.java
+++ JavaFX Application/src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label label;
+
+    public void intialize(){
+    }
+
+    @FXML
+    public void handleMouseEnter(){
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit(){
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
 }
