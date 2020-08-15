fa0b42374ceb8bb3d84c352a85301d727993c307
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-08-15 12:00:38 -0700

    Connected table view to 'App'

--- GUIDemo/src/sample/FXMLDocumentController.java
+++ GUIDemo/src/sample/FXMLDocumentController.java
@@ -1,14 +1,34 @@
 package sample;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXMLDocumentController implements Initializable {
+
+
+    // change scene
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("ExampleOfTableView.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        // This line gets the stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+
+    }
 
     // These items are for the ListView and TextArea examples
     @FXML private ListView listView;
